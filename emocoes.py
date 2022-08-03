9714d502dfcdc73ec028483045b78e74a809ba61
DeskFanzin <gabriel.jardel.matheus@gmail.com>
2022-08-03 13:53:10 -0300

    deu

--- emocoes.py
+++ emocoes.py
@@ -10,19 +10,20 @@ while(1):
     cv2.imshow("Video", frame)
     
     #codigo do indiano
-    result = DeepFace.analyze(frame,actions=['emotion'], enforce_detection=False)
+    result = DeepFace.analyze(frame, actions=['emotion'], enforce_detection=False)
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = faceCascade.detectMultiScale(gray,1.1,4)
 
     for(x, y, w, h) in faces:
-      cv2.rectangle(frame, (x,y),(x+w, y+h),(0,255,0),2) 
+      cv2.rectangle(frame, (x,y),(x+w, y+h),(0,255,0),2)
 
     font = cv2.FONT_HERSHEY_SIMPLEX
 
     cv2.putText(frame, result['dominant_emotion'], (50,50), font, 3, (255,0,0), 2, cv2.LINE_4)
 
+    cv2.imshow("Video", frame)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
